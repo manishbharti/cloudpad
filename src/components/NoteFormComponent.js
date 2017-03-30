f7a7dc5e3fbbc1a6653a19aec3d8b71db86280eb
Manish Bharti <manish.bharti@appdirect.com>
2017-03-30 16:29:46 +0530

    Note save functionality added

--- src/components/NoteFormComponent.js
+++ src/components/NoteFormComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {ControlLabel, Form, FormControl, FormGroup} from "react-bootstrap";
+import {Button, ControlLabel, Form, FormControl, FormGroup} from "react-bootstrap";
 import jquery from "jquery";
 import {UserData} from "../App";
 
@@ -20,7 +20,7 @@ class NoteFormComponent extends Component {
     render() {
         if (this.state.show) {
             return (
-                <Form>
+                <Form onSubmit={this._login.bind(this)}>
                     <FormGroup controlId="formControlsNoteName">
                         <ControlLabel>Notepad Name</ControlLabel>
                         <FormControl type="text" placeholder="Enter notepad Name" value={this.state.note.name || ''}
@@ -28,10 +28,11 @@ class NoteFormComponent extends Component {
                     </FormGroup>
                     <FormGroup controlId="formControlsTextarea">
                         <ControlLabel>Notepad</ControlLabel>
-                        <FormControl componentClass="textarea" placeholder="Write notes here" rows="25"
+                        <FormControl componentClass="textarea" placeholder="Write notes here" rows="10"
                                      value={this.state.note.content || ''}
                                      onChange={this._handleContentChange.bind(this)}/>
                     </FormGroup>
+                    <Button type="submit" bsStyle="primary" className="pull-right">Save</Button>
                 </Form>
             );
         } else {
@@ -64,6 +65,28 @@ class NoteFormComponent extends Component {
             dataType: "json",
             success: function (response) {
                 self.setState({note: {id: response.id, name: response.name, content: response.content}, show: true});
+            },
+            error: function (xhr, status) {
+                console.info("Error");
+            }
+        });
+    }
+
+    _login(event) {
+        event.preventDefault();
+        let self = this;
+        jquery.ajax({
+            url: "http://localhost:8080/notepad/" + this.state.note.id,
+            type: "PUT",
+            crossDomain: true,
+            beforeSend: function (request) {
+                request.setRequestHeader('Authorization', UserData.getAccessToken());
+            },
+            data: JSON.stringify({"name": this.state.note.name, "content": this.state.note.content}),
+            contentType: 'application/json; charset=utf-8',
+            dataType: "json",
+            success: function (response) {
+                alert("Notepad updated successfully.");
             },
             error: function (xhr, status) {
                 console.info("Error");

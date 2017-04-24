fec9b8c38f92751d3b527e2835c0b6072328731d
Manish Bharti <manish.bharti@appdirect.com>
2017-04-24 17:58:22 +0530

    Note save redirect to dashboard implementation

--- src/components/protected/NoteForm.js
+++ src/components/protected/NoteForm.js
@@ -2,10 +2,18 @@ import React, {Component} from "react";
 import {ref} from "../../config/constants";
 
 export default class NoteForm extends Component {
-    state = {
-        id: this.props.match.params.id,
-        note: {}
-    };
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: props.match.params.id,
+            note: {}
+        };
+
+        this._handleFormTextChange = this._handleFormTextChange.bind(this);
+        this._submitForm = this._submitForm.bind(this);
+    }
 
     componentWillMount() {
         if (this.state.id) {
@@ -20,13 +28,13 @@ export default class NoteForm extends Component {
         return (
             <div className="container">
                 <div className="row">
-                    <form className="contact-form" onSubmit={this._saveNote.bind(this)}>
+                    <form className="contact-form" onSubmit={this._submitForm}>
                         <div className="row">
                             <div className="col-md-12">
                                 <div className="form-group">
                                     <input type="text" className="form-control" name="name" placeholder="Notepad Name"
                                            value={this.state.note.name || ''}
-                                           onChange={this._handleFormTextChange.bind(this)}/>
+                                           onChange={this._handleFormTextChange}/>
                                 </div>
                             </div>
                         </div>
@@ -35,7 +43,7 @@ export default class NoteForm extends Component {
                                 <div className="form-group">
                                     <textarea className="form-control textarea" rows="10" name="content"
                                               value={this.state.note.content || ''}
-                                              placeholder="Content" onChange={this._handleFormTextChange.bind(this)}/>
+                                              placeholder="Content" onChange={this._handleFormTextChange}/>
                                 </div>
                             </div>
                         </div>
@@ -56,16 +64,36 @@ export default class NoteForm extends Component {
         this.setState({note: note});
     }
 
-    _saveNote(event) {
+    _submitForm(event) {
         event.preventDefault();
+        if (this.state.id) {
+            this._updateNote(this.state.id, this.state.note);
+        } else {
+            this._saveNote(this.state.note);
+        }
+    }
 
+    _saveNote(note) {
         let id = ref.push().getKey();
-        let note = this.state.note;
-
         ref.child(`notes/${id}`).set({
             id: id,
             name: note.name,
             content: note.content
-        });
+        }).then(() => this._goToDashboard());
+    }
+
+    _updateNote(id, note) {
+        let updates = {};
+        updates['/notes/' + id + '/name'] = note.name;
+        updates['/notes/' + id + '/content'] = note.content;
+        ref.update(updates).then(() => this._goToDashboard());
+    }
+
+    _goToDashboard() {
+        this.props.history.push('/dashboard')
     }
 }
+
+NoteForm.propTypes = {
+    history: React.PropTypes.object
+};

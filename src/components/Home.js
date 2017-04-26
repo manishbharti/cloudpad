737f8bda11b8f380ef0f57cf3606d2091e53c3a8
Manish Bharti <manish.bharti@appdirect.com>
2017-04-26 10:56:57 +0530

    Home.js redirect to dashboard if user is loggedin

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 import React, {Component} from "react";
+import {firebaseAuth} from "../config/constants";
+import {Redirect} from "react-router-dom";
 
 export default class Home extends Component {
+    state = {
+        user: null
+    };
+
+    componentWillMount() {
+        this.setState({user: firebaseAuth().currentUser});
+    }
+
     render() {
         return (
             <div>
+                {this.state.user ? <Redirect to="/dashboard"/> : null}
                 <h3>Welcome to Cloudpad</h3>
                 <p>A online notepad facility provider</p>
                 <p>Login and enjoy :-)</p>
